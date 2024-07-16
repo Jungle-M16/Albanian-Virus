c27ae562576c908a07f261fd78b14ddfaee57547
Ari Björn Ólafsson <ari+dev@dynamo.is>
2024-07-16 10:44:21 +0000

    Update Albanian Virus.ps1

--- Albanian Virus.ps1
+++ Albanian Virus.ps1
@@ -1,8 +1,8 @@
 Add-Type -AssemblyName System.Windows.Forms
 
 $result = [System.Windows.Forms.MessageBox]::Show(
-    "Hi, I am an Albanian virus but because of poor technology in my country unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation! Best regards, Albanian virus",
-    "Virus Alert",
+    "Hi, I am an Albanian virus but because of poor technology in my country unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation! Best regards,Albanian virus",
+    "Virus Alert !",
     [System.Windows.Forms.MessageBoxButtons]::YesNoCancel,
     [System.Windows.Forms.MessageBoxIcon]::Warning
 )
